9063be7ca0bdb8979b9fb3fae766a594c7720ad7
David Dyball <daviddyball@users.noreply.github.com>
2015-06-03 14:43:14 +0100

    Adding support for overriding the Erlang Cookie
    `docker_entrypoint.sh` script now supports overriding the Erlang cookie via the environment variable `ERLANG_COOKIE`

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,9 @@
 set -e
 
 if [ "$1" = 'rabbitmq-server' ]; then
+	if [ "${ERLANG_COOKIE}" -n ]; then
+		echo ${ERLANG_COOKIE} > /var/lib/rabbitmq/.erlang.cookie
+	fi
 	chown -R rabbitmq /var/lib/rabbitmq
 	set -- gosu rabbitmq "$@"
 fi
