b853b78106f1641bf332a77e9ae5a6c28bf2d9c8
David Dyball <daviddyball@users.noreply.github.com>
2015-06-03 15:00:54 +0100

    Syntax and permission fix on ERLANG_COOKIE check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 if [ "$1" = 'rabbitmq-server' ]; then
-	if [ "${ERLANG_COOKIE}" -n ]; then
+	if [ -n "${ERLANG_COOKIE}" ]; then
 		echo ${ERLANG_COOKIE} > /var/lib/rabbitmq/.erlang.cookie
+		chmod 600 /var/lib/rabbitmq/.erlang.cookie
 	fi
 	chown -R rabbitmq /var/lib/rabbitmq
 	set -- gosu rabbitmq "$@"
