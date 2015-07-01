aae4d2b9773419a7421e413337068b32feb4995a
Tianon Gravi <admwiggin@gmail.com>
2015-07-01 10:50:32 -0700

    Improve ".erlang.cookie" file management by adding RABBITMQ_ERLANG_COOKIE for specifying it directly
    This allows for direct, simple control over the contents of the `.erlang.cookie` file, which allows for much simpler container interconnections.
    
    This also symlinks that file into `/root` so that the root user will pick it up properly without additional changes.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+if [ "$RABBITMQ_ERLANG_COOKIE" ]; then
+	cookieFile='/var/lib/rabbitmq/.erlang.cookie'
+	if [ -e "$cookieFile" ]; then
+		if [ "$(cat "$cookieFile" 2>/dev/null)" != "$RABBITMQ_ERLANG_COOKIE" ]; then
+			echo >&2
+			echo >&2 "warning: $cookieFile contents do not match RABBITMQ_ERLANG_COOKIE"
+			echo >&2
+		fi
+	else
+		echo "$RABBITMQ_ERLANG_COOKIE" > "$cookieFile"
+		chmod 600 "$cookieFile"
+		chown rabbitmq "$cookieFile"
+	fi
+fi
+
 if [ "$1" = 'rabbitmq-server' ]; then
 	configs=(
 		# https://www.rabbitmq.com/configure.html
