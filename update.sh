962e6663ea11698be00c53f2eeb0da40f5551647
Joe Ferguson <joe@infosiftr.com>
2019-02-01 13:54:08 -0800

    Fix update.sh to have a erlang and openssl versions for rabbitmq 3.8

--- update.sh
+++ update.sh
@@ -12,12 +12,14 @@ versions=( "${versions[@]%/}" )
 # https://www.rabbitmq.com/which-erlang.html ("Maximum supported Erlang/OTP")
 declare -A otpMajors=(
 	[3.7]='21'
+	[3.8]='21'
 )
 
 # https://www.openssl.org/policies/releasestrat.html
 # https://www.openssl.org/source/
 declare -A opensslMajors=(
 	[3.7]='1.1'
+	[3.8]='1.1'
 )
 
 # TODO will these always be signed by Matt Caswell? (https://www.openssl.org/community/omc.html)
